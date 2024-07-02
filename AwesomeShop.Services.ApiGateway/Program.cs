5ff182ef69abc046ea3397c5d8d45167e7f40675
Thiago <thiagooffice2@gmail.com>
2024-07-02 00:21:46 -0300

    feat: creating API Gateway project and add Ocelot

---
+++ AwesomeShop.Services.ApiGateway/Program.cs
@@ -0,0 +1,35 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot();
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", false)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddJsonFile("configuration.json")
+    .AddEnvironmentVariables();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+await app.UseOcelot();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
